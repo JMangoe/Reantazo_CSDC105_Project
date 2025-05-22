af04615a6f45df9c2fed3c5d78fe6440569102eb
John Marc S. Reantazo <jmreantazo@gbox.adnu.edu.ph>
2025-05-23 01:43:44 +0800

    improved intro page

--- client/src/pages/IntroPage.js
+++ client/src/pages/IntroPage.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function IntroPage(){
     return (
-        <div className="intro-page">
+        <div className="intro-page fade-in">
             <h1>Welcome to BroQuote Blog</h1>
             <p>Write. Reflect. Rebuild</p>
             <Link to="/blogs" className='btn'>Enter</Link>
