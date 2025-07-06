a390e2b2e5a575a2ae26dfb131d9e97b58ff42c9
John Marc S. Reantazo <jmreantazo@gbox.adnu.edu.ph>
2025-07-06 22:42:58 +0800

    removed the word about in date since inconsistent

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -4,7 +4,8 @@ export function getSmartDate(dateStr) {
   const createdDate = new Date(dateStr);
   const now = new Date();
   const diffInDays = Math.floor((now - createdDate) / (1000 * 60 * 60 * 24));
+
   return diffInDays < 7
-    ? `${formatDistanceToNow(createdDate, { addSuffix: true })}`
+    ? `${formatDistanceToNow(createdDate, { addSuffix: true, roundingMethod: "floor" })}`
     : format(createdDate, 'MMM d, yyyy');
 }
